01384c7b77a2124845abeb97ca3e7ea9bf65cb27
kibadatova-netizen <kibadatova@gmail.com>
2025-11-10 20:31:26 -0600

    Updated Main.java: changed console message and x2

--- src/Practice.java
+++ src/Practice.java
@@ -2,5 +2,8 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("Hello, this is my second commit");
         System.out.println("bye, my second commit is finish");
+        System.out.println("bye, my second commit is finish");
+
+
     }
 }
