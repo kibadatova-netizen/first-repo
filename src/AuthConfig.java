8bdf0b210bf36003fe92b378961b5bcd252a3441
kibadatova-netizen <kibadatova@gmail.com>
2025-11-11 18:23:43 -0600

    Last changes

--- src/AuthConfig.java
+++
@@ -1,7 +0,0 @@
-public class AuthConfig {
-    public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Login logic!");
-        }
-    }
-}
--- src/day_1_first_some_logic/Practice.java
+++ src/day_1_first_some_logic/Practice.java
@@ -1,3 +1,5 @@
+package day_1_first_some_logic;
+
 public class Practice {
     public static void main(String[] args) {
         System.out.println("Hello, this is my second commit");
