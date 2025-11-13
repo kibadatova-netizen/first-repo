2141a49182ae60feb5fd14f1c405b07f6758edc4
kibadatova-netizen <kibadatova@gmail.com>
2025-11-12 19:02:34 -0600

    deleted Student.java

--- src/day_1_first_some_logic/Student.java
+++
@@ -1,9 +0,0 @@
-package day_1_first_some_logic;
-
-public class Student {
-    public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Hello student");
-        }
-    }
-}
